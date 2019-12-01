552f0573c14c9f8020ca04a3ca7e6f3f9e245446
xlgg <877133646@qq.com>
2019-12-01 14:47:22 +0800

    add sout

--- src/main/java/com/baizhi/easypoi/EasypoiApplication.java
+++ src/main/java/com/baizhi/easypoi/EasypoiApplication.java
@@ -8,6 +8,7 @@ public class EasypoiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EasypoiApplication.class, args);
+        System.out.println("llll");
     }
 
 }
