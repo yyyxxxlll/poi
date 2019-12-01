98946ad4cb299f3c648ae487cd965112bd2946d4
yyyxxxlll <34063182+yyyxxxlll@users.noreply.github.com>
2019-12-01 14:50:29 +0800

    Update EasypoiApplication.java

--- src/main/java/com/baizhi/easypoi/EasypoiApplication.java
+++ src/main/java/com/baizhi/easypoi/EasypoiApplication.java
@@ -9,6 +9,7 @@ public class EasypoiApplication {
     public static void main(String[] args) {
         SpringApplication.run(EasypoiApplication.class, args);
         System.out.println("llll");
+        System.out.println("llll");
     }
 
 }
